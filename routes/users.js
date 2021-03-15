bcf8fa98765f96c226982a450434f5d8e2061d4a
Lokeon <kevin.lopezcala99@gmail.com>
2021-03-15 19:49:38 +0100

    :penguin: patch password

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 const User = require("../model/User");
 const verify = require("./verifyToken");
 
@@ -17,6 +18,24 @@ router.get("/", verify, async (req, res) => {
   try {
     const users = await User.find();
     res.send(users);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// PATCH - Change User's password
+router.patch("/password/:id", verify, async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+    const updateUser = await User.updateOne(
+      { _id: req.params.id },
+      {
+        $set: { password: hashPassword },
+      }
+    );
+    res.send(updateUser);
   } catch (error) {
     res.status(400).send(error);
   }
